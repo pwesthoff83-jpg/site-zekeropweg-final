34a6c338935e06b3f4ee7ebcd1b05393a0a9f50f
pwesthoff83-jpg <p.westhoff83@gmail.com>
2025-12-01 22:07:58 +0100

    Update scripts.js

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -7,4 +7,9 @@ document.addEventListener('DOMContentLoaded', ()=> {
 function toggleMenu() {
     document.getElementById("mainNav").classList.toggle("open");
 }
+function toggleMenu() {
+    const nav = document.getElementById("mainNav");
+    nav.classList.toggle("open");
+}
+
 
