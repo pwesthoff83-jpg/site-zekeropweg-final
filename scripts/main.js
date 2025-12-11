6ce3830fd11aed4aee620010da0b164ddf3446a7
pwesthoff83-jpg <p.westhoff83@gmail.com>
2025-12-11 22:52:57 +0100

    Update main.js

--- scripts/main.js
+++ scripts/main.js
@@ -1,1 +1,10 @@
+function toggleMenu() {
+    document.getElementById("mobileMenu").classList.add("open");
+    document.getElementById("mobileMenuOverlay").classList.add("active");
+}
+
+function closeMenu() {
+    document.getElementById("mobileMenu").classList.remove("open");
+    document.getElementById("mobileMenuOverlay").classList.remove("active");
+}
 
